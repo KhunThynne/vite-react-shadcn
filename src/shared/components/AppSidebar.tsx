31e298ef82a14d87dfc2a2fd5765f7faff14d072
Khunthynne <khunthynne@gmail.com>
2026-01-20 18:27:29 +0700

    fix: sidebar error

--- src/shared/components/AppSidebar.tsx
+++ src/shared/components/AppSidebar.tsx
@@ -1,55 +1,59 @@
-'use client'
-import { useTranslation } from 'react-i18next'
-import { Home, LayoutDashboard, Settings } from 'lucide-react'
-import { NavLink } from 'react-router-dom'
-import { cn } from '@components/ui/utils'
-import { Avatar, AvatarFallback, AvatarImage } from '@components/ui/avatar'
-import clsx from 'clsx'
-import { useSidebarContext } from '../contexts/ProviderSidebar'
-import { SwitchThemeButton } from './SwitchThemeButton'
-import { SwitchLanguageButton } from './SwitchLanguageButton'
+"use client";
+import { useTranslation } from "react-i18next";
+import { Home, LayoutDashboard, Settings } from "lucide-react";
+
+import { cn } from "@components/ui/utils";
+import { Avatar, AvatarFallback, AvatarImage } from "@components/ui/avatar";
+import clsx from "clsx";
+import { useSidebarContext } from "../contexts/ProviderSidebar";
+import { SwitchThemeButton } from "./SwitchThemeButton";
+import { SwitchLanguageButton } from "./SwitchLanguageButton";
+import { Link } from "@tanstack/react-router";
 
 export function AppSidebar() {
-  const { t } = useTranslation()
-  const { open } = useSidebarContext()
+  const { t } = useTranslation();
+  const { open } = useSidebarContext();
   const navItems = [
-    { icon: LayoutDashboard, label: t('Sidebar.dashboard'), to: '/' },
-    { icon: Settings, label: t('Sidebar.settings'), to: '/settings' }
-  ]
+    { icon: LayoutDashboard, label: t("Sidebar.dashboard"), to: "/" },
+    { icon: Settings, label: t("Sidebar.settings"), to: "/settings" },
+  ];
 
   return (
     <aside
       className={clsx(
         ` border-r bg-muted/40 lg:block  lg:static absolute  top-0 bottom-0 z-20 transition-all duration-300 ease-in-out`,
-        open ? 'left-0' : '-left-100'
+        open ? "left-0" : "-left-100",
       )}
     >
       <div className="flex h-full  max-lg:w-xs flex-col border-r bg-card ">
         <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6 ">
-          <NavLink to="/" className="flex items-center gap-2 font-semibold">
+          <Link to="/" className="flex items-center gap-2 font-semibold">
             <div className="flex h-8 w-8 items-center justify-center rounded-lg bg-primary text-primary-foreground">
               <Home className="size-5" />
             </div>
-            <span>{t('Sidebar.app_name')}</span>
-          </NavLink>
+            <span>{t("Sidebar.app_name")}</span>
+          </Link>
         </div>
         <div className="flex-1 overflow-auto py-4">
           <nav className="grid gap-1 px-2">
-            {navItems.map((item, index) => (
-              <NavLink
-                key={index}
-                to={item.to}
-                className={({ isActive }) =>
-                  cn(
-                    'flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:bg-muted hover:text-primary',
-                    isActive ? 'bg-muted text-primary' : 'text-muted-foreground'
-                  )
-                }
-              >
-                <item.icon className="h-4 w-4" />
-                {item.label}
-              </NavLink>
-            ))}
+            {navItems.map((item, index) => {
+              const isActive = item.to === location.pathname;
+              return (
+                <Link
+                  key={index}
+                  to={item.to}
+                  className={cn(
+                    "flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:bg-muted hover:text-primary",
+                    isActive
+                      ? "bg-muted text-primary"
+                      : "text-muted-foreground",
+                  )}
+                >
+                  <item.icon className="h-4 w-4" />
+                  {item.label}
+                </Link>
+              );
+            })}
           </nav>
         </div>
         <div className="p-4 flex justify-end gap-2">
@@ -70,5 +74,5 @@ export function AppSidebar() {
         </div>
       </div>
     </aside>
-  )
+  );
 }
